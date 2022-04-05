86dfc2303b664d2dbbc8ea158d5287f08d3903db
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-04-05 11:56:40 +0200

    fix: allow number as timestamp (will be converted)

--- src/interfaces/createTokenEvent.interface.ts
+++ src/interfaces/createTokenEvent.interface.ts
@@ -2,7 +2,7 @@ export interface CreateTokenEvent {
   _id: string
   blockNumber: number
   unixTimestamp: number
-  timestamp: Date
+  timestamp: Date | number
   transactionHash: string
   createdAt: Date
   updatedAt: Date
