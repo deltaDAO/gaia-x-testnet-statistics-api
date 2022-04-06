f5083efd24f61522238a527a5767fb324d0f2d2d
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-04-06 10:35:47 +0200

    feat(fetchBlocks): get latest block from provider + improve logger.info status

--- src/utils/fetchBlocks.ts
+++ src/utils/fetchBlocks.ts
@@ -1,31 +1,13 @@
 import { ethers } from 'ethers'
 import Block from '../models/block.model'
-import fetch from 'node-fetch'
 import { logger } from './logger'
 
 const providerURL = process.env.PROVIDER_URL || 'https://rpc.gaiaxtestnet.oceanprotocol.com' // or use your local node 'http://localhost:8545'
 const provider = new ethers.providers.JsonRpcProvider(providerURL)
 
-function getLatestBlockNumber() {
-  return fetch('https://exchangelog.minimal-gaia-x.eu/chain-blocks', {
-    credentials: 'omit',
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0',
-      Accept: 'application/json, text/javascript, */*; q=0.01',
-      'Accept-Language': 'en-US,en;q=0.5',
-      'X-Requested-With': 'XMLHttpRequest',
-      'Sec-Fetch-Dest': 'empty',
-      'Sec-Fetch-Mode': 'cors',
-      'Sec-Fetch-Site': 'same-origin',
-      'Sec-GPC': '1',
-      'Cache-Control': 'max-age=0'
-    },
-    referrer: 'https://exchangelog.minimal-gaia-x.eu/',
-    method: 'GET',
-    mode: 'cors'
-  })
-    .then(response => response.json())
-    .then(data => data.blocks[0].block_number)
+async function getLatestBlockNumber() {
+  const latestBlock = await provider.getBlock('latest')
+  return latestBlock.number
 }
 
 async function getLatestBlockNumberFromDb() {
@@ -41,15 +23,15 @@ export async function fetchBlocks(bundleSize: number) {
   logger.info('==== Start Block import ====')
   try {
     const latestBlockNumberInDb = await getLatestBlockNumberFromDb()
-    const lastBlockNumber = await getLatestBlockNumber()
+    const latestBlockNumber = await getLatestBlockNumber()
 
-    logger.info(`Latest Block in DB: ${latestBlockNumberInDb} Lastest Block: ${lastBlockNumber}`)
+    logger.info(`Latest Block in DB: ${latestBlockNumberInDb} Lastest Block: ${latestBlockNumber}`)
 
     let blockArray = []
     let blockNumberStart = latestBlockNumberInDb ? latestBlockNumberInDb + 1 : 0
     let blockNumberEnd = blockNumberStart + bundleSize - 1
 
-    for (let currentBlockNumber = blockNumberStart; currentBlockNumber < lastBlockNumber; currentBlockNumber++) {
+    for (let currentBlockNumber = blockNumberStart; currentBlockNumber < latestBlockNumber; currentBlockNumber++) {
       if (currentBlockNumber > blockNumberEnd) {
         const newBlockNumberEnd = blockNumberEnd + bundleSize
         await Block.insertMany(blockArray)
@@ -57,14 +39,14 @@ export async function fetchBlocks(bundleSize: number) {
         blockArray = []
 
         blockNumberStart = blockNumberStart + bundleSize
-        blockNumberEnd = newBlockNumberEnd > lastBlockNumber ? lastBlockNumber : newBlockNumberEnd
+        blockNumberEnd = newBlockNumberEnd > latestBlockNumber ? latestBlockNumber : newBlockNumberEnd
       }
 
       const newBlock = await getBlock(currentBlockNumber)
 
       const { number, timestamp, transactions }: { number: number; timestamp: number; transactions: string[] } = newBlock
 
-      logger.info(`Fetch Block: ${number}`)
+      logger.info(`Fetch Block: ${number.toLocaleString('en-US')} / ${latestBlockNumber.toLocaleString('en-US')}`)
 
       blockArray.push({
         blockNumber: number,
