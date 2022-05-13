d1a8f4031defbe4aa1745a4c8dcbf053a6abe02e
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-05-13 11:58:57 +0200

    feat: add boolean return value to indicate successful slack notification

--- src/services/slack.service.ts
+++ src/services/slack.service.ts
@@ -1,19 +1,22 @@
 import fetch from 'node-fetch'
 import { logger } from '../utils/logger'
 
-function sendNotification(text: string) {
+async function sendNotification(text: string): Promise<boolean> {
   const body = {
     text
   }
   try {
-    fetch(process.env.SLACK_WEBHOOK_SECRET_URL, {
+    const response = await fetch(process.env.SLACK_WEBHOOK_SECRET_URL, {
       method: 'post',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' }
     })
+    if (response.ok) return true
+    return false
   } catch (error) {
     // Important: don't expose SLACK_WEBHOOK_SECRET_URL to logs
     logger.error('Failed to send slack notification.')
+    return false
   }
 }
 
