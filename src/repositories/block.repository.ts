737a38ad7b675c80ee53ff887f4f5d66e8088ebd
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-05-17 12:43:40 +0200

    fix: change import to right interface name

--- src/repositories/block.repository.ts
+++ src/repositories/block.repository.ts
@@ -1,5 +1,5 @@
 import Block from '../models/block.model'
-import { Block as IBlock } from 'interfaces/block.interface'
+import { IBlock } from 'interfaces/block.interface'
 
 export function getLatestBlocks(numberOfBlocks: number): Promise<IBlock[]> {
   return Block.find({}).sort({ blockNumber: -1 }).limit(numberOfBlocks).exec()
