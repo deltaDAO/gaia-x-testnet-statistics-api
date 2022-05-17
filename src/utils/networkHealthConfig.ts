e61dee76b42e46bb88da8600cafee12aa61e2df8
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-05-17 10:12:00 +0200

    feat: add healthcheck parameters to .env

--- src/utils/networkHealthConfig.ts
+++ src/utils/networkHealthConfig.ts
@@ -1,10 +1,22 @@
 import { NetworkHealthConfig } from 'interfaces/networkHealthConfig.interface'
 
 export const networkHealthConfig: NetworkHealthConfig = {
-  numberOfBlocksToAnalyze: 720, // number of latest blocks which will be analyzed, expected 720 Blocks/hour
-  targetBlockTimeInSeconds: 5, // specifies the target blocktime for a healthy network
-  minAllowedAverageBlocktime: 4.8, // an average below that will trigger a notification
-  maxAllowedAverageBlocktime: 5.2, // an average above that will trigger a notification
-  allowedSingleBlockTimeDeviationInSeconds: 0, // a deviation bigger than this value will mark a block as deviated
-  allowedNumberDeviatedBlocks: 5 // if this threshold of deviated blocks is surpassed a notification will be sent
+  // number of latest blocks which will be analyzed, expected 720 Blocks/hour
+  numberOfBlocksToAnalyze: Number(process.env.HEALTH_NUMBER_OF_BLOCKS) || 720,
+
+  // specifies the target blocktime for a healthy network
+  targetBlockTimeInSeconds: Number(process.env.HEALTH_TARGET_BLOCK_TIME) || 5,
+
+  // an average below that will trigger a notification
+  minAllowedAverageBlocktime: Number(process.env.HEALTH_MIN_AVG_BLOCK_TIME) || 4.8,
+
+  // an average above that will trigger a notification
+  maxAllowedAverageBlocktime: Number(process.env.HEALTH_MAX_AVG_BLOCK_TIME) || 5.2,
+
+  // a deviation bigger than this value will mark a block as deviated
+  allowedSingleBlockTimeDeviationInSeconds:
+    process.env.HEALTH_ALLOWED_DEVIATION_SECS === undefined ? 0 : Number(process.env.HEALTH_ALLOWED_DEVIATION_SECS),
+
+  // if this threshold of deviated blocks is surpassed a notification will be sent
+  allowedNumberDeviatedBlocks: process.env.HEALTH_ALLOWED_DEVIATED_BLOCKS === undefined ? 5 : Number(process.env.HEALTH_ALLOWED_DEVIATED_BLOCKS)
 }
