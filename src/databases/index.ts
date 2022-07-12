ad090ade1c1514342136c8b776f00987537b9924
3xz1 <alex@delta-dao.com>
2022-07-12 13:50:15 +0200

    Added MongoDB Authentication

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -4,7 +4,7 @@ import { dbConfig } from '../interfaces/db.interface'
 const { host, port, database }: dbConfig = config.get('dbConfig')
 
 export const dbConnection = {
-  url: `mongodb://${process.env.MONGO_HOST || host}:${process.env.MONGO_PORT || port}/${process.env.MONGO_DATABASE || database}`,
+  url: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_HOST || host}:${process.env.MONGO_PORT || port}/${process.env.MONGO_DATABASE || database}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
