8aca892831a01e44c72069326720b8e369243616
Alexander Eger <40068802+3xz1@users.noreply.github.com>
2022-07-12 14:33:16 +0200

    Update index.ts

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,10 +1,10 @@
 import config from 'config'
 import { dbConfig } from '../interfaces/db.interface'
 
-const { host, port, database }: dbConfig = config.get('dbConfig')
+const { host, port, database, username, password }: dbConfig = config.get('dbConfig')
 
 export const dbConnection = {
-  url: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.MONGO_HOST || host}:${process.env.MONGO_PORT || port}/${process.env.MONGO_DATABASE || database}`,
+  url: `mongodb://${process.env.DB_USERNAME || username}:${process.env.DB_PASSWORD || password}@${process.env.MONGO_HOST || host}:${process.env.MONGO_PORT || port}/${process.env.MONGO_DATABASE || database}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
