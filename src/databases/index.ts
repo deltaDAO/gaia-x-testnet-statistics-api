c38d49625ef4fface9e477ee096509608e28b8bb
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-08-02 16:22:58 +0200

    feat: upgrade to mongoose version 6

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -4,10 +4,7 @@ import { dbConfig } from '../interfaces/db.interface'
 const { host, port, database, username, password }: dbConfig = config.get('dbConfig')
 
 export const dbConnection = {
-  url: `mongodb://${process.env.DB_USERNAME || username}:${process.env.DB_PASSWORD || password}@${process.env.MONGO_HOST || host}:${process.env.MONGO_PORT || port}/${process.env.MONGO_DATABASE || database}`,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }
+  url: `mongodb://${process.env.DB_USERNAME || username}:${process.env.DB_PASSWORD || password}@${process.env.MONGO_HOST || host}:${
+    process.env.MONGO_PORT || port
+  }/${process.env.MONGO_DATABASE || database}?authSource=admin`
 }
