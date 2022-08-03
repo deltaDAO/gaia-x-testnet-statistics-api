2ee5ec62f429db79bd1e8ec8e51e2fd0b74b18a8
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-08-03 10:43:58 +0200

    feat: add mongodb authSource to option

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -6,5 +6,6 @@ const { host, port, database, username, password }: dbConfig = config.get('dbCon
 export const dbConnection = {
   url: `mongodb://${process.env.DB_USERNAME || username}:${process.env.DB_PASSWORD || password}@${process.env.MONGO_HOST || host}:${
     process.env.MONGO_PORT || port
-  }/${process.env.MONGO_DATABASE || database}?authSource=admin`
+  }/${process.env.MONGO_DATABASE || database}`,
+  options: { authSource: 'admin' }
 }
